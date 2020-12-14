ef64e72a1809be3f6abdda3ac392776836df2cec
Andrew Coutts <andrewjohncoutts@gmail.com>
2020-12-13 20:10:35 -0500

    mobile framework is building now

--- ios/Classes/SwiftFlutterLndPlugin.swift
+++ ios/Classes/SwiftFlutterLndPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import Lndmobile
 
 public class SwiftFlutterLndPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -7,8 +8,60 @@ public class SwiftFlutterLndPlugin: NSObject, FlutterPlugin {
     let instance = SwiftFlutterLndPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
-
+  
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch(call.method) {
+    case "startLndService": startLndService(call, result: result)
+    default: result(FlutterMethodNotImplemented)
+    }
+  }
+  
+  // Start the lnd service. lnddir should be a directory we can write to, like getApplicationDocumentsDirectory() from Dart.
+  private func startLndService(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    guard let arguments = call.arguments as? [String:Any] else {
+        result(FlutterError(code: "ARGUMENTS", message: "Invalid arguments supplied", details: nil))
+        return
+    }
+    
+    // Verify lnddir supplied
+    let lnddir: String = (arguments["lnddir"] as? String) ?? ""
+    if (lnddir.isEmpty) {
+      result(FlutterError(code: "ARGUMENTS", message: "Must supply a valid string for lnddir", details: nil))
+      return
+    }
+    
+    LndmobileStart("--lnddir=\(lnddir) --bitcoin.testnet", LndUnlockerReady(), LndRpcReady())
+  }
+}
+
+// The unlockerReady callback is called when the WalletUnlocker service is
+// ready.
+class LndUnlockerReady: LndmobileCallback {
+  override func onError(_ error: Error?) {
+    print("LndUnlockerReady error: \(error?.localizedDescription ?? "")")
+    
+    // TODO: emit stream events for subscribed dart listeners
+  }
+  
+  override func onResponse(_ data: Data?) {
+    print("LndUnlockerReady response")
+    
+    // TODO: emit stream events for subscribed dart listeners
+  }
+}
+
+// The rpcReady callback is called after the wallet has been unlocked and lnd is
+// ready to accept RPC calls
+class LndRpcReady: LndmobileCallback {
+  override func onError(_ error: Error?) {
+    print("LndRpcReady error: \(error?.localizedDescription ?? "")")
+    
+    // TODO: emit stream events for subscribed dart listeners
+  }
+  
+  override func onResponse(_ data: Data?) {
+    print("LndRpcReady response")
+    
+    // TODO: emit stream events for subscribed dart listeners
   }
 }
--- lib/flutter_lnd.dart
+++ lib/flutter_lnd.dart
@@ -1,14 +1,16 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 
 class FlutterLnd {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_lnd');
+  static const MethodChannel _channel = const MethodChannel('flutter_lnd');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<String?> get platformVersion async {
+    final workingDir = await getApplicationDocumentsDirectory();
+
+    await _channel.invokeMethod('startLndService', {
+      'lnddir': workingDir.path,
+    });
   }
 }
